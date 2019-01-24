19ca35f2959c9e01ffe064e4ba2f1bbcd71ba61e
DLMousey <tom@llamatorials.com>
2019-01-24 09:45:24 +0000

    Add photo & tag models

--- ig_private_api/api/models.py
+++ ig_private_api/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    id = models.UUIDField()
+    description = models.TextField()
+    image_path = models.TextField()
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+
+
+class Tag(models.Model):
+    id = models.UUIDField()
+    name = models.CharField(max_length=16)
+    photo = models.ForeignKey(Photo, on_delete=models.DO_NOTHING, related_name='tags')
